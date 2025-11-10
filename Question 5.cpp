4c7808c78cfbf1aa287dc9d7e18e7933c2e39710
niharikachawla24 <nchawla_be24@thapar.edu>
2025-11-10 06:59:22 +0530

    Update Question 5.cpp

--- Question 5.cpp
+++ Question 5.cpp
@@ -1,45 +1,51 @@
 #include <iostream>
-struct Node{
+using namespace std;
+
+struct Node {
     int data;
     Node* next;
 };
 
-Node* head=NULL; 
-Node* current= head;
+Node* head = NULL;
+Node* current = head;
 
- void create_node(int x,Node* &head,Node* &current){
-     Node* NewNode=new Node; 
-     NewNode->data=x;
-     NewNode->next=NULL;
-     
-     if(head==NULL){
-         head=NewNode;
-    current=head; } else 
-     {current->next=NewNode; 
-        current=current->next;  }
-      
-      }
+void create_node(int x, Node*& head, Node*& current) {
+    Node* newNode = new Node;
+    newNode->data = x;
+    newNode->next = NULL;
 
-bool isCircular(Node* head){ Node* slow=head;
-Node* fast=head;
-
-if (head==NULL) 
-        return false;
-        
-while(fast!=NULL&&fast->next!=NULL){ 
-    
-    slow=slow->next;
-    fast=fast->next->next;
-   
-    
-    if (slow==fast){ 
-        return true; } 
-} return false;
+    if (head == NULL) {
+        head = newNode;
+        current = head;
+    } else {
+        current->next = newNode;
+        current = current->next;
     }
-    
-int main() { create_node(3,head,current);
-create_node(4,head,current);
-create_node(5,head,current);
-current->next=head;
-std::cout<<isCircular(head);
+}
+
+bool isCircular(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+
+    if (head == NULL)
+        return false;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
+}
+
+int main() {
+    create_node(3, head, current);
+    create_node(4, head, current);
+    create_node(5, head, current);
+    current->next = head;
+    cout << isCircular(head);
+    return 0;
 }
